0cd1afe87e2bf1bfde4d4a36eac0592971caca88
cleiron <pinchonclaire@gmail.com>
2025-02-22 16:29:18 +0100

    calcul air

--- ex01.c
+++ ex01.c
@@ -1,100 +1,51 @@
 #include <stdio.h>
-#define PI 3.14159
+#define PI 3.1415
 
+int calcul_aire_rectangle (int longeur, int largeur)
+{
+    return longeur*largeur;
+}
 
-int main (void);
+int calcul_aire_triangle (int longeur, int largeur)
+{
+    return longeur*largeur/2;
+}
 
-
- int aireDuCarre (int cote)
+int calcul_aire_carre (int cote)
 {
     return cote*cote;
-
 }
 
- int aireDuRectangle (int longueur, int largeur)
-
+float calcul_aire_cercle (int rayon)
 {
-    return longueur * largeur;
-
-}
- double aireDuTriangle (double base, double hauteur)
-
- {
-    return base * hauteur / 2.0;
-
- }
-
- double aireDuCercle (double rayon)
-
- {
-    return PI * rayon * rayon;
-
- }
-
- void calculAire(char forme)
- {
-    if (forme == 'c')
-    {else if (forme == 'r')}
-
-    {else if (forme == 't')}
-    
-    {else if (forme == 'o')}
- 
-
- }
-
-if (forme == 'c')
-
-{
-    int cote;
-    printf("entre la longueur du cote du carre : ");
-    scanf("%d", &cote);
-    printf("L'aire du carre est : %d\n", aireDuCarre(cote));
-
+    return (rayon*rayon*PI);
 }
 
-else if ( forme == 'r')
-{
-    int longueur, largeur;
-printf("Entre la longueur et la largeur du rectangle : ");
-scanf("%d %d" , &longueur, &largeur);
-printf("L'aire du rectangle est : %d\n", aireDuRectangle(longueur, largeur));
-
-}
-
-else if ( forme == 't')
-{
-    double base, hauteur;
-    printf("Entre la base et la hauteur du triangle : ");
-    scanf("%1f %1f" , &base, & hauteur);
-    printf("L'aire du triangle est : %.21f\n", aireDuTriangle (base, hauteur));
-
-}
-
-else if ( forme == 'o')
-{
-    double rayon;
-    printf("Entre le rayon du cercle : ");
-    scanf("%1f", &rayon);
-    printf("L'aire du cercle est : %.21f\n", aireDuCercle (rayon));
-
-}
-else
-{
-    printf("commande invalide, choisis c, r, t, ou o");
-}
 
 int main ()
 {
-    char forme;
-    printf("choisis une forme geometrique : \n");
-    printf("c pour carre\n");
-    printf("r pour rectangle");
-    printf("t pour triangle");
-    printf("o pour cercle");
-    printf("ton choix : ");
-    scanf("%c", &forme);
 
-    calculAire (forme);
+    int longeur;
+    int largeur;
+    int cote;
+    int rayon;
+
+   
+    printf("Quel est la longeur ?\n");
+    scanf("%d",&longeur);
+    printf("Quel est la largeur ?\n");
+    scanf("%d",&largeur);
+    printf("Quel est la longeur du cote ?\n");
+    scanf("%d",&cote);
+    printf("Quel est le rayon ?\n");
+    scanf("%d",&rayon);
+    
+
+
+    printf("L'aire du rectangle est : %d\n", calcul_aire_rectangle(longeur,largeur));
+    printf("L'aire du triangle est : %d\n", calcul_aire_triangle(longeur,largeur));
+    printf("L'aire du carre est : %d\n", calcul_aire_carre(cote));
+    printf("L'aire du cercle est : %f\n", calcul_aire_cercle(rayon));
+
     return 0;
 }
