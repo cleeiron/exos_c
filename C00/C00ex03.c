08cd290f96abdd46649c671279a91079ccc43722
cleiron <pinchonclaire@gmail.com>
2025-06-17 13:43:25 +0200

    3

--- C00/C00ex03.c
+++ C00/C00ex03.c
@@ -4,10 +4,26 @@ void ft_print_numbers(void);
 {
     int chiffre = 0;
     char chiffre_ascii;
-    while(chiffre<=9){
+
+    while(chiffre<=9)
+    {
         chiffre_ascii= chiffre +'0';
         write(1, &chiffre_ascii,1);
         chiffre ++;
     }
     write(1, "\n", 1);
+}
+void ft_is_negative(int n);
+{
+    int n = 9;
+    char n_ascii;
+
+    while(n >= 0)
+    {
+    n_ascii = n +'0';
+    write(1, &n_ascii, 1);
+        n--;
+}
+    write(1, "\n", 1);
+
 }
